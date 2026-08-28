e2e5fb527ded4f2b10da988468ae131ea21addf8
dnflguswls086-sudo <dnflguswls086@gmail.com>
2026-08-28 12:20:11 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,24 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBIDqCYP4g1rJMhCkoBJsvIU7oApGGSzZ4",
+  authDomain: "nancho-3095f.firebaseapp.com",
+  projectId: "nancho-3095f",
+  storageBucket: "nancho-3095f.firebasestorage.app",
+  messagingSenderId: "1007162375203",
+  appId: "1:1007162375203:web:21e4307049ffb50ff28596",
+  measurementId: "G-YH050RBEXJ"
+};
+
+export const APP_CONFIG = {
+  title: "난초반 주말 누리집",
+  timezone: "Asia/Seoul",
+  openSchedule: {
+    "2026-08-28": { start: 11, end: 20, label: "8월 28일(금) 오전 11시 ~ 오후 8시" },
+    "2026-08-29": { start: 9,  end: 20, label: "8월 29일(토) 오전 9시 ~ 오후 8시" },
+    "2026-08-30": { start: 9,  end: 20, label: "8월 30일(일) 오전 9시 ~ 오후 8시" }
+  },
+  maxPhotos: 3,
+  maxPhotoBytes: 5 * 1024 * 1024,
+  teacherUid: "d0J60LOC3BShK0X8faSGZd9gOE63",
+  students: ["강승효", "김동윤", "김주원", "양지완", "이서준", "이영현", "이이안", "이지환", "이호진", "정민준", "정윤후", "하이준", "고서진", "김리재", "박나윤", "박서은", "송윤하", "이가빈", "임엘린", "정단우", "정이현", "조서윤", "최인아", "김나현"],
+  studentEmails: {"강승효": "n01@nancho.invalid", "김동윤": "n02@nancho.invalid", "김주원": "n03@nancho.invalid", "양지완": "n04@nancho.invalid", "이서준": "n05@nancho.invalid", "이영현": "n06@nancho.invalid", "이이안": "n07@nancho.invalid", "이지환": "n08@nancho.invalid", "이호진": "n09@nancho.invalid", "정민준": "n10@nancho.invalid", "정윤후": "n11@nancho.invalid", "하이준": "n12@nancho.invalid", "고서진": "n13@nancho.invalid", "김리재": "n14@nancho.invalid", "박나윤": "n15@nancho.invalid", "박서은": "n16@nancho.invalid", "송윤하": "n17@nancho.invalid", "이가빈": "n18@nancho.invalid", "임엘린": "n19@nancho.invalid", "정단우": "n20@nancho.invalid", "정이현": "n21@nancho.invalid", "조서윤": "n22@nancho.invalid", "최인아": "n23@nancho.invalid", "김나현": "n24@nancho.invalid"}
+};
